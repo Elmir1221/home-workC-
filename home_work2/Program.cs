4260b108a2199d82f2c8bab32268c5021dbe2247
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-08 00:33:45 +0400

    Add files via upload

---
+++ home_work2/Program.cs
@@ -0,0 +1,14 @@
+﻿//n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin hasilini hesablayın.
+
+
+int a = 20;
+int b = 12;
+int Sum = 0;
+
+if (a % 2 == 0 && b % 2 == 0);
+{
+
+    Sum = a + b;
+
+};
+Console.WriteLine(Sum);
