6ed86270282135a259460a4471d034eb07395d15
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-08 00:36:11 +0400

    Add files via upload

---
+++ home_work4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+int[] nums = {1,2,3,4,5,6 };
+int Count = 0;
+for (int i = 0; i <= nums.Length; i++)
+{
+
+    if (i % 2 == 1)
+    {
+
+        Count += i;
+
+    }
+        
+     
+
+}
+
+Console.WriteLine(Count);
+
+
---
+++ home_work5/Program.cs
@@ -0,0 +1,19 @@
+﻿//Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+int[] nums = { 1, 2, 3, 4, 5, 6 };
+int Count = 0;
+
+for (int i=0; i<= nums.Length; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Count+=i ;
+    
+    
+    }
+        
+    
+
+}
+Console.WriteLine(Count);
