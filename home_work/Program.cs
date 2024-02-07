6ad4f1b6da4cf477f87e79741d2f33dd4466e4d3
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-08 00:32:44 +0400

    Add files via upload

---
+++ home_work/Program.cs
@@ -0,0 +1,12 @@
+﻿//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+using System.ComponentModel.Design;
+
+int n = 20;
+
+if( n%3==0 && n%7==0);
+{
+    Console.WriteLine("bölünmür");
+
+};
+
