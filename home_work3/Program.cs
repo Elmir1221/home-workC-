145959cc2dc1d869132ed6be8dad8cac69acc0ed
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-08 00:34:53 +0400

    Add files via upload

---
+++ home_work3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+
+int n = 1;
+int m = 13;
+int Sum = 0;
+
+for(int i=1; i<=13; i++)
+{
+
+    if(i%2==1)
+    {
+
+        Sum += i;
+
+
+    }
+}
+
+
+Console.WriteLine(Sum);
